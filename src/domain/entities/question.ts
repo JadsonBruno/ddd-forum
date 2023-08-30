8cdc446c1228f57f7c845bc6384f4beca444a421
Jadson <jadsombruno2016@gmail.com>
2023-08-30 15:42:45 -0300

    feat: create slug value object

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,9 +1,11 @@
 import { randomUUID } from "crypto";
+import { Slug } from './value-objects/slug';
 
 interface QuestionProps {
     title: string;
     content: string;
     authorId: string;
+    slug: Slug;
 }
 
 export class Question {
@@ -11,12 +13,14 @@ export class Question {
     public title: string;
     public content: string;
     public authorId: string;
+    public slug: Slug;
 
     constructor (props: QuestionProps, id?: string) {
-        const  { title , content, authorId } = props;
+        const  { title , content, authorId, slug } = props;
         this.title = title;
         this.content = content;
         this.authorId = authorId;
+        this.slug = slug;
         this.id = id ?? randomUUID();
     }
 }
