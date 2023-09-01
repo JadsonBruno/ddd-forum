af56548cbd5a43178817b7023a91087c74f5023d
Jadson <jadsombruno2016@gmail.com>
2023-09-01 15:50:03 -0300

    feat: add static method to creation

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityId } from "../../core/entities/unique-entity-id";
 import { Answer } from "../entities/answer";
 import {AnswersRepository} from '../repositories/answers-repository';
 
@@ -11,10 +12,10 @@ export class AnswerQuestionUseCase {
     constructor (private answersRepository: AnswersRepository) {}
 
     async execute ({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
-        const answer = new Answer({
-            authorId: instructorId,
+        const answer = Answer.create({
+            authorId: new UniqueEntityId(instructorId),
             content,
-            questionId
+            questionId: new UniqueEntityId(questionId)
         });
 
         await this.answersRepository.create(answer);
